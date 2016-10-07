fcbe7565bc488cc96567f1cf7eccd2cf6e6b4b79
2998550097 <2998550097@qq.com>
2016-10-07 13:16:05 +0800

    修改了实体类Test--tt

--- yunduan_git/src/main/java/com/wym/entity/Test.java
+++ yunduan_git/src/main/java/com/wym/entity/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	private int num;
 	private String name;
 	
+	private int count;
+	
 	
 	public int getNum() {
 		return num;
@@ -17,6 +19,11 @@ public class Test {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
 	
 }
