eb8439504d8311257fe029b8b67f1d1982cb47ad
Horizen1123 <1181699039@qq.com>
2016-10-07 13:18:12 +0800

    修改了Test实体类--wym

--- yunduan_git/src/main/java/com/wym/entity/Test.java
+++ yunduan_git/src/main/java/com/wym/entity/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	private int num;
 	private String name;
 	
+	private Integer count;
+	
 	
 	public int getNum() {
 		return num;
@@ -16,6 +18,12 @@ public class Test {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
 	}
 	
 	
